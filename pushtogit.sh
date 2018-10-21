8629581d2ecc7e5af3b5e0dd0c3a46e2a7d5161e
mansetagunj <gunjmanseta@gmail.com>
2018-10-21 12:27:58 -0600

    Udating pushtogit file

--- pushtogit.sh
+++ pushtogit.sh
@@ -1,2 +1,3 @@
 print $1
-git add ./ && git commit -m "automated update push"  && git push origin master
+git add ./ && git commit && git push origin master
+#git add ./ && git commit -m "automated update push"  && git push origin master
